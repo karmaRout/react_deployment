5de275f3eca63735c7b991f1375243d1b979d26d
imprintnext-karma <kenton@imprintnext.com>
2026-04-16 05:07:24 +0530

    force workflow detection

--- react-dep/src/App.jsx
+++ react-dep/src/App.jsx
@@ -5,11 +5,11 @@ import heroImg from './assets/hero.png'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(1)
 
   return (
     <>
-      HELLO 
+      HELLO mm
     </>
   )
 }
